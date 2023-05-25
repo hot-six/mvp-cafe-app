9a6e4adbe949cc6dc32585ed9c3858a1b8a70cc6
Parksangwoo <psu8430@gmail.com>
2023-05-26 04:41:49 +0900

    Feat: Setting Firebase Analysistics

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nado_client_mvp/app/binding.dart';
@@ -7,10 +8,15 @@ import 'package:nado_client_mvp/app/core/values/theme.dart';
 import 'package:nado_client_mvp/app/global_widgets/not_found.dart';
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.instance;
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: firebaseAnalytics),
+      ],
       title: '나도 손님',
       initialRoute: Routes.CLIENT_PAGE,
       initialBinding: AppBinding(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nado_client_mvp/app/data/provider/storage_provider.dart';
+import 'package:nado_client_mvp/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app/app.dart';
 
 Future<void> _initService() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   final prefs = await SharedPreferences.getInstance();
   if (prefs.getBool('is_first_run') ?? true) {
     Storage storage = Storage();
